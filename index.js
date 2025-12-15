a41ecec7358f96e59ffae263565014adac385969
xiaogao <15290171627@163.com>
2025-12-15 14:06:16 +0800

    feat: 初始化项目

---
+++ index.js
@@ -0,0 +1,11 @@
+const Koa = require('koa')
+const app = new Koa()
+const a = 0
+try {
+    const port = process.env.PORT || 8080
+    const host = process.env.IP || '0.0.0.0'
+    app.listen(port, host)
+    console.log(`Server is Running on port:${port}`);
+} catch (e) {
+    console.error(e);
+}
