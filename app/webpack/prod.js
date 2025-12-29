c5c0958feeb4a2385377b63b7a913708565cf054
xiaogao <15290171627@163.com>
2025-12-30 00:42:40 +0800

    fix: webpack多线程打包happypack改为thread-loader

--- app/webpack/prod.js
+++ app/webpack/prod.js
@@ -1,16 +1,21 @@
-const webpack = require('webpack')
-const webBaseConfig=require('./config/webpack.prod.js')
-console.log(`\nbuilding... \n`)
-webpack(webBaseConfig,(err,status)=>{
-    if(err){
-        console.log(err)
-        return
-    }
-    process.stdout.write(`${status.toString({
-        colors:true, // 在控制台输出色彩信息
-        modules:false, // 不显示每个模块的打包信息
-        children:false, // 不显示子编译任务信息
-        chunks:false, // 不显示每个代码块信息 
-        chunkModules:true // 显示代码中模块的信息
-    })}`)
-})
+// 隐藏 Node.js 弃用警告，使构建输出更清晰
+process.noDeprecation = true;
+
+const webpack = require("webpack");
+const webBaseConfig = require("./config/webpack.prod.js");
+console.log(`\nbuilding... \n`);
+webpack(webBaseConfig, (err, status) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  process.stdout.write(
+    `${status.toString({
+      colors: true, // 在控制台输出色彩信息
+      modules: false, // 不显示每个模块的打包信息
+      children: false, // 不显示子编译任务信息
+      chunks: false, // 不显示每个代码块信息
+      chunkModules: true, // 显示代码中模块的信息
+    })}`
+  );
+});
